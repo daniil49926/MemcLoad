38561f8f9225b6ee61b4f0809766dd35ac2c6d4c
kaMata0 <daniil49926@gmail.com>
2022-11-17 19:46:22 +0300

    black and isort refactor. add ci.

--- single_threaded_load/cache_load.py
+++ single_threaded_load/cache_load.py
@@ -1,18 +1,22 @@
+import collections
+import datetime
 import glob
 import gzip
 import logging
 import os
 import sys
-import datetime
-import redis
-import collections
-from optparse import OptionParser
 from copy import deepcopy
-from typing import Dict, Tuple, Any
+from optparse import OptionParser
+from typing import Any, Dict, Tuple
+
+import redis
+
 from load_env import load_environ
 
 load_environ()
-AppsInstalled = collections.namedtuple("AppsInstalled", ["dev_type", "dev_id", "lat", "lon", "apps"])
+AppsInstalled = collections.namedtuple(
+    "AppsInstalled", ["dev_type", "dev_id", "lat", "lon", "apps"]
+)
 NORMAL_ERR_RATE = 0.01
 
 
@@ -23,10 +27,10 @@ def get_redis_conn(key_and_port: Dict[str, int]) -> Tuple[Dict[str, redis.Redis]
     while err:
         for key, port in key_and_port.items():
             conn_dict[key] = redis.Redis(
-                username=os.environ.get('REDIS_LOGIN'),
-                password=os.environ.get('REDIS_PWD'),
-                host=os.environ.get('REDIS_HOST'),
-                port=port
+                username=os.environ.get("REDIS_LOGIN"),
+                password=os.environ.get("REDIS_PWD"),
+                host=os.environ.get("REDIS_HOST"),
+                port=port,
             )
             try:
                 conn_dict[key].ping()
@@ -64,36 +68,40 @@ def parse_appsinstalled(line: str) -> AppsInstalled | None:
 
 
 def establish_all_connections() -> Dict[str, redis.Redis]:
-    redis_conn, err = get_redis_conn({
-        'idfa': int(os.environ.get('REDIS_idfa')),
-        'gaid': int(os.environ.get('REDIS_gaid')),
-        'adid': int(os.environ.get('REDIS_adid')),
-        'dvid': int(os.environ.get('REDIS_dvid'))
-    })
+    redis_conn, err = get_redis_conn(
+        {
+            "idfa": int(os.environ.get("REDIS_idfa")),
+            "gaid": int(os.environ.get("REDIS_gaid")),
+            "adid": int(os.environ.get("REDIS_adid")),
+            "dvid": int(os.environ.get("REDIS_dvid")),
+        }
+    )
     if err:
-        logging.error('Redis is not ok')
+        logging.error("Redis is not ok")
         sys.exit(1)
-    logging.info('Redis is ok')
+    logging.info("Redis is ok")
     return redis_conn
 
 
 def load_in_redis(redis_conn: Dict[str, redis.Redis], app_line: AppsInstalled) -> bool:
     key = "%s:%s" % (app_line.dev_type, app_line.dev_id)
-    redis_conn[app_line.dev_type].set(key, f'{app_line.lat}:{app_line.lon}-{app_line.apps}')
+    redis_conn[app_line.dev_type].set(
+        key, f"{app_line.lat}:{app_line.lon}-{app_line.apps}"
+    )
     return True
 
 
 def main(options: Any) -> None:
     redis_connection_pools = establish_all_connections()
-    for fn in glob.iglob('../' + options.pattern):
+    for fn in glob.iglob("../" + options.pattern):
         processed = errors = 0
-        logging.info('Processing %s' % fn)
+        logging.info("Processing %s" % fn)
         fd = gzip.open(fn)
         for line in fd:
             line = line.strip()
             if not line:
                 continue
-            appsinstalled = parse_appsinstalled(line.decode('utf-8'))
+            appsinstalled = parse_appsinstalled(line.decode("utf-8"))
             if not appsinstalled:
                 errors += 1
                 continue
@@ -111,21 +119,23 @@ def main(options: Any) -> None:
         if err_rate < NORMAL_ERR_RATE:
             logging.info("Acceptable error rate (%s). Successfull load" % err_rate)
         else:
-            logging.error("High error rate (%s > %s). Failed load" % (err_rate, NORMAL_ERR_RATE))
+            logging.error(
+                "High error rate (%s > %s). Failed load" % (err_rate, NORMAL_ERR_RATE)
+            )
         fd.close()
         dot_rename(fn)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     op = OptionParser()
     op.add_option("--pattern", action="store", default="/data/appsinstalled/*.tsv.gz")
     (opts, args) = op.parse_args()
     logging.basicConfig(
         level=logging.INFO,
-        format='[%(asctime)s] %(levelname).1s %(message)s',
-        datefmt='%Y.%m.%d %H:%M:%S'
+        format="[%(asctime)s] %(levelname).1s %(message)s",
+        datefmt="%Y.%m.%d %H:%M:%S",
     )
     start = datetime.datetime.now()
     main(opts)
-    logging.info(f'Run time: {datetime.datetime.now() - start}')
+    logging.info(f"Run time: {datetime.datetime.now() - start}")
     # Process two tsv.gz by 512mb = 52.15 min
